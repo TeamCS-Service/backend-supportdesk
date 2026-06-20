a176fafc206f1f6d40a2ffd1a6dce6cebcb8a270
timcss <css.ldwt@gmail.com>
2026-06-21 04:23:43 +0700

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,11 +1,8 @@
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
-    database: process.env.DB_NAME || 'myproject',
-    password: process.env.DB_PASSWORD || 'Gacor1234',
-    port: process.env.DB_PORT || 5432,
+    connectionString: process.env.DATABASE_URL,
+    ssl: { rejectUnauthorized: false }
 });
 
 module.exports = pool;
